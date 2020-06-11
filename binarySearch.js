8922be75160b7808dae4f62c6379fbdc405c1ad9
Brian Holt <bharrisonholt@gmail.com>
2020-06-11 10:56:08 -0700

    binarysearch redo

--- binarySearch.js
+++ binarySearch.js
@@ -2,28 +2,27 @@
 
 const sortedArray=[1,2,3,4,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
 
-let findMe=11;
+let findMe=31;
 
-const binarySearch= (arr,target) => {
-  let min=0;
-  let max=arr.length;
-  let pivot;
+const binarySearch = (array,target) => {
+  let min = 0;
+  let max = array.length-1;
 
   while (min <= max){
-    //each cycle pivot gets cut in half
-    pivot = Math.floor(min + (max-min)/2);
-  if (arr[pivot] === target){
-    return pivot
-    } 
-  if (target > arr[pivot]) {
-    min = pivot + 1 
-  } 
-  if ( target < arr[pivot]){
-     max = pivot -1
+    let pivot = Math.floor((min + max)/2);
+
+    if (target === array[pivot]){
+      return pivot
+    }
+    if (target > array[pivot]){
+      min = pivot +1;
+    }
+    if (target < array[pivot]){
+      max = pivot -1;
     }
   }
-  return -1
+  return -1;
 }
+
+
 console.log(binarySearch(sortedArray,findMe));
-
-
